5a57458a1878e47897aa36d922fadaff76102be1
litovkaartem <litovka@neo31.ru>
2024-12-05 08:11:34 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, jsonify
-import random
-
+from flask import Flask, render_template, jsonify, request
+import serial
+import time
 app = Flask(__name__)
-
 # Главная страница
 @app.route('/')
 def home():
@@ -11,21 +10,45 @@ def home():
 # Генерация случайных значений
 @app.route('/random_values')
 def random_values():
-    # Генерируем рандомные значения для 5 сенсоров
-    values = {
-        "sen1": random.randint(1, 100),
-        "sen2": random.randint(1, 100),
-        "sen3": random.randint(1, 100),
-        "sen4": random.randint(1, 100),
-        "sen5": random.randint(1, 100),
-    }
-    return jsonify(values)
-#@app.route('/run-script')
-#def run_script():
-    # Кнопка обновить
-    
+    PORT = "/dev/ttyACM0"
+    BAUD_RATE = 9600
+    sen1 = sen2 = sen3 = sen4 = sen5 = 0
 
+    ser = serial.Serial(PORT, BAUD_RATE, timeout=1)
+    print(f"Соединение установлено на {PORT} со скоростью {BAUD_RATE}")
+    time.sleep(2)  # Даем время на инициализацию Serial
+
+    while True:
+        # Если Arduino отправляет данные, считываем строку
+        if ser.in_waiting > 0:
+            data = ser.readline().decode('utf-8').strip()  # Считываем строку
+            metrix = data.split(",")
+            if len(metrix) == 5:
+                sen1, sen2, sen3, sen4, sen5 = map(int, metrix)
+
+        # Передаем полученные значения с Arduino
+        values = {
+            "sen1": sen1,
+            "sen2": sen2,
+            "sen3": sen3,
+            "sen4": sen4,
+            "sen5": sen5,
+        }
+        return jsonify(values)
+@app.route('/run-script', methods=['POST','GET'])
+def run_script():
+    # Кнопка обновить
+    PORT = "/dev/ttyACM0"
+    BAUD_RATE = 9600
+    try:
+        ser = serial.Serial(PORT, BAUD_RATE, timeout=1)
+        command = request.args.get('command', 'c')  # Исправлено: использование request.args.get
+        ser.write(command.encode())  # Отправляем команду
+        print(f"Отправлена команда: {command}")
+        return 'Скрипт успешно выполнен!', 200  # Возврат строки с HTTP-кодом 200
+    except Exception as e:
+        print(f"Ошибка отправки команды: {e}")
+        return f"Произошла ошибка: {str(e)}", 500
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
 
